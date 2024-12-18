8a07df592a8d6173296cd672ea39b55c5e85d1d3
logitechsabarni <sabarni.guha2024@uem.edu.in>
2024-12-18 22:01:10 +0530

    Add files via upload

---
+++ Arduino project.ino
@@ -0,0 +1,16 @@
+void setup() {
+  pinMode(13,OUTPUT);
+
+  // put your setup code here, to run once:
+  
+}
+
+
+void loop() {
+  digitalWrite(13,HIGH);
+  delay(69);
+  digitalWrite(13,LOW);
+  delay(69);
+  // put your main code here, to run repeatedly:
+ 
+}
